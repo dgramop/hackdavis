6fbde4f44129542ab36203e57731c924db1f0115
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2024-04-28 19:57:31 -0700

    minor changes

--- client_rs/src/main.rs
+++ client_rs/src/main.rs
@@ -3,6 +3,8 @@ use std::io::Write;
 use gilrs::{Axis, Button, Event, EventType, Gilrs};
 use serialport::{SerialPort, SerialPortBuilder};
 
+const DIFF: f32 = 0.0; 
+
 fn main() {
 
     let mut gilrs = Gilrs::new().unwrap();
@@ -16,20 +18,24 @@ fn main() {
 
     let mut sp = serialport::new("/dev/tty.usbserial-B0004H6P", 57600).open_native().expect("Serial Port");
 
+    let mut flaperon_trim: f32 = 0.0;
+    let mut roll: f32 = 0.0;
+
     loop {
         // Examine new events
         while let Some(Event { id, event, time }) = gilrs.next_event() {
-            println!("{:?} New event from {}: {:?}", time, id, event);
+          //  println!("{:?} New event from {}: {:?}", time, id, event);
             active_gamepad = Some(id);
             match event {
                 EventType::AxisChanged(Axis::LeftStickY, pos, _) => {
                     println!("Throttle is {pos}");
-                    sp.write(&[12, 0, (1.0+(19.0*(pos+1.0)/2.0)) as u8]).expect("failed to write");
+                   // sp.write(&[12, 0, (1.0+(19.0*(pos+1.0)/2.0)) as u8]).expect("failed to write");
+                    sp.write(&[12, 0, 0]).expect("failed to write");
                 },
                 EventType::AxisChanged(Axis::LeftStickX, pos, _) => {
                     println!("FlaperonRoll is {pos}");
-                    sp.write(&[12, 1, (1.0+(19.0*(pos+1.0)/2.0)) as u8]).expect("failed to write");
-                    sp.write(&[12, 2, (20.0-(19.0*(pos+1.0)/2.0)) as u8]).expect("failed to write");
+                    sp.write(&[12, 1, (16.0-DIFF-(10.0*(pos+1.0)/2.0)) as u8]).expect("failed to write");
+                    sp.write(&[12, 2, (16.0-DIFF-(10.0*(pos+1.0)/2.0)) as u8]).expect("failed to write");
                 },
                 EventType::AxisChanged(Axis::RightStickY, pos, _) => {
                     println!("FlaperonTrim is {pos}");
